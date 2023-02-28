a90df35d5bf0e676ec5eeff471ace3aa131e5cc8
davidvandang <98755624+davidvandang@users.noreply.github.com>
2023-02-28 17:25:46 +0000

    Online button

--- app/src/main/java/com/example/fallapplication3/HomeFragment.java
+++ app/src/main/java/com/example/fallapplication3/HomeFragment.java
@@ -1,19 +1,62 @@
 package com.example.fallapplication3;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 
 public class HomeFragment extends Fragment {
 
+    private TextView statusTextView;
+    private Button deactivateButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Initialize views
+        statusTextView = view.findViewById(R.id.text_center);
+        deactivateButton = view.findViewById(R.id.button_center2);
+
+        // Set initial status
+        statusTextView.setText("Online");
+        statusTextView.setBackgroundResource(R.drawable.circular_button);
+        deactivateButton.setText("Deactivate");
+        deactivateButton.setBackgroundResource(R.drawable.rounded_rectangle_button);
+
+        // Set button click listener
+        deactivateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (statusTextView.getText().equals("Online")) {
+                    // Change the status text and color
+                    statusTextView.setText("Offline");
+                    statusTextView.setBackgroundResource(R.drawable.red_circular_button);
+
+                    // Change the button text and color
+                    deactivateButton.setText("Activate");
+                    deactivateButton.setBackgroundResource(R.drawable.green_rounded_rectangle_button);
+                } else {
+                    // Change the status text and color
+                    statusTextView.setText("Online");
+                    statusTextView.setBackgroundResource(R.drawable.circular_button);
+
+                    // Change the button text and color
+                    deactivateButton.setText("Deactivate");
+                    deactivateButton.setBackgroundResource(R.drawable.rounded_rectangle_button);
+                }
+            }
+        });
+
+        return view;
     }
 }
